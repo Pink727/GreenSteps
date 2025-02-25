1b5bf78257de069cc5e91737ece65a3b15744c64
Pink727 <doc72789@gmail.com>
2025-02-25 14:53:50 -0800

    Update import statement for Home page to use TypeScript file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home';
+import Home from '../pages/Home.tsx';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
 import ErrorPage from '../pages/ErrorPage';
