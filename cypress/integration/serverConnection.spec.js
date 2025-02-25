1d31152b01a3527e497b1ab9d4f0776bef3fb870
Pink727 <doc72789@gmail.com>
2025-02-25 14:35:22 -0800

    Refactor server connection test for clarity and consistency

--- cypress/integration/serverConnection.spec.js
+++ cypress/integration/serverConnection.spec.js
@@ -1,9 +1,9 @@
 describe('Server Connection Test', () => {
     it('should connect to the server and return a successful response', () => {
-      cy.request('http://localhost:3001') // Adjust the URL to match your server's URL and port
+      cy.request('http://localhost:3001') 
         .then((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body).to.include('MongoDB connected successfully'); // Adjust the expected response as needed
+          expect(response.body).to.include('MongoDB connected successfully');
         });
     });
   });
