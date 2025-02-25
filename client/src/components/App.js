5254d4d3ce86650888f9962c212a1cff5641478e
Pink727 <doc72789@gmail.com>
2025-02-25 15:14:10 -0800

    Update routing in App component to use Routes instead of Switch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 import Profile from '../pages/Profile';
@@ -9,12 +9,12 @@ const App = () => {
     return (
         <Router>
             <div>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/profile" component={Profile} />
-                    <Route path="/Errorpage" component={ErrorPage} />
-                </Switch>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/Errorpage" element={<ErrorPage />} />
+                </Routes>
             </div>
         </Router>
     );
