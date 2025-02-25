e6db9b24a59cdb6214bd9037ee61d6cf80998d58
Pink727 <doc72789@gmail.com>
2025-02-25 14:57:57 -0800

    Fix import statements for Home and Login components to ensure correct file extensions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../pages/Home.tsx';
-import Login from '../pages/Login';
+import Home from '../pages/Home';
+import Login from '../pages/Login.tsx';
 import Profile from '../pages/Profile';
 import ErrorPage from '../pages/ErrorPage';
 
