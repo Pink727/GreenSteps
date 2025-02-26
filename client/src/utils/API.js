3ff84d9f3461bccd9910fe0c43d4af2538da8267
Pink727 <doc72789@gmail.com>
2025-02-26 11:10:27 -0800

    Add user registration functionality to API utility

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -53,3 +53,13 @@ export const authenticateUser = async (credentials) => {
         throw error;
     }
 };
+
+export const registerUser = async (userData) => {
+    try {
+        const response = await axios.post(`${API_URL}/auth/register`, userData);
+        return response.data;
+    } catch (error) {
+        console.error('Error registering user:', error);
+        throw error;
+    }
+};
