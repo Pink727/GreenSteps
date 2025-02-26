dccf273fc429872108993140d4af1d4e936d96f8
Pink727 <doc72789@gmail.com>
2025-02-25 16:41:24 -0800

    Fix import statement for typeDefs in index.js to use named import syntax

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,5 +1,5 @@
 import { gql } from 'apollo-server-express';
-import typeDefs from './typeDefs.js';
+import { typeDefs } from './typeDefs.js';
 import resolvers from './resolvers.js';
 
 const schema = {
