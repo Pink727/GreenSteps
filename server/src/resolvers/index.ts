4d98c5d93f72ae8aab60531c2d8200ddd43fa1c6
Pink727 <doc72789@gmail.com>
2025-02-27 14:35:27 -0800

    Add context parameter to GraphQL resolvers for improved functionality

--- server/src/resolvers/index.ts
+++ server/src/resolvers/index.ts
@@ -7,19 +7,19 @@ const userController = new UserController();
 
 export const resolvers: IResolvers<any, any> = {
   Query: {
-    getUser: async (_: any, { id }: { id: string }) => {
-      return await userController.getUser(id);
+    getUser: async (_: any, { id }: { id: string }, context: any) => {
+      return await userController.getUser(id, context);
     },
   },
   Mutation: {
-    registerUser: async (_: any, { userInput }: { userInput: any }) => {
-      return await authController.registerUser(userInput);
+    registerUser: async (_: any, { userInput }: { userInput: any }, context: any) => {
+      return await authController.registerUser(userInput, context);
     },
-    loginUser: async (_: any, { email, password }: { email: string; password: string }) => {
-      return await authController.loginUser(email, password);
+    loginUser: async (_: any, { email, password }: { email: string; password: string }, context: any) => {
+      return await authController.loginUser(email, password, context);
     },
-    updateUser: async (_: any, { id, userInput }: { id: string; userInput: any }) => {
-      return await userController.updateUser(id, userInput);
+    updateUser: async (_: any, { id, userInput }: { id: string; userInput: any }, context: any) => {
+      return await userController.updateUser(id, userInput, context);
     },
   },
 };
