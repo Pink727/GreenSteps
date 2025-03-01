102dea97245d382744bf675cdb16aa7cac3a254e
Kevin Noguera <noguera.ke@gmail.com>
2025-03-01 17:52:22 -0500

    init Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,46 @@
 import React from "react";
-import styles from "./Footer.css";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <p>&copy; {new Date().getFullYear()} Travel Guide. All Rights Reserved.</p>
-        <p>© 2025</p>
-        <ul>
-          <li>
-            <a href="/Contact Us" className={styles.link}>
-              Contact Us
-            </a>
-          </li>
-        </ul>
-      </div>
-    </footer>
+    <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h5> GreenSteps</h5>
+                <div class="row">
+<p>GreenSteps is a mobile application that helps you track your carbon footprint and rewards you for making sustainable choices.</p> 
+                </div>
+
+                <ul class="nav">
+                    <li class="nav-item"><a href="" class="nav-link pl-0"><i class="fa fa-facebook fa-lg"></i></a></li>
+                    <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-twitter fa-lg"></i></a></li>
+                    <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-github fa-lg"></i></a></li>
+                    <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-instagram fa-lg"></i></a></li>
+                </ul>
+                <br></br>
+            </div>
+            <div class="col-md-2">
+                <h5 class="text-md-right">Contact Us</h5>
+                <br></br>
+            </div>
+            <div class="col-md-5">
+                <form>
+                    <fieldset class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                    </fieldset>
+                    <fieldset class="form-group">
+                        <textarea class="form-control" id="exampleMessage" placeholder="Message"></textarea>
+                    </fieldset>
+                    <fieldset class="form-group text-xs-right">
+                        <button type="button" class="btn btn-secondary-outline btn-lg">Send</button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</footer>
+
   );
 };
 
