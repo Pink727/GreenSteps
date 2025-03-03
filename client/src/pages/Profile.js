1ebba15570cf6982652cd6a65e9bbd05c9e7e4cd
Pink727 <doc72789@gmail.com>
2025-03-03 15:37:34 -0800

    refactor: simplify Profile component by removing user data handling and adding development notice

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,55 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Profile.css';
 
 const Profile = () => {
-    const [username, setUsername] = useState('');
-    const [points, setPoints] = useState(0);
-
-    useEffect(() => {
-        const fetchUserData = async () => {
-            const userData = await fetch('/api/user').then(res => res.json());
-            setUsername(userData.username);
-            setPoints(userData.points);
-        };
-
-        fetchUserData();
-    }, []);
-
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
-    };
-
-    const handlePointsChange = (e) => {
-        setPoints(e.target.value);
-    };
-
-    const handleSave = () => {
-        const updatedUserData = { username, points };
-        fetch('/api/user', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updatedUserData),
-        });
-    };
-
     return (
         <div style={{ textAlign: 'center', marginTop: '50px' }}>
             <h1>Profile</h1>
-            <div>
-                <label>
-                    Username:
-                    <input type="text" value={username} onChange={handleUsernameChange} />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Points:
-                    <input type="number" value={points} onChange={handlePointsChange} />
-                </label>
-            </div>
-            <button onClick={handleSave}>Save</button>
+            <p>This feature is under development. Please check back later.</p>
         </div>
     );
 };
