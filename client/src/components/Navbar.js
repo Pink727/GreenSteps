81f8466ddd118a3b77171a44c640a7cdda0ff92c
Kevin Noguera <noguera.ke@gmail.com>
2025-03-03 10:02:28 -0500

    navbar update to link with images

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "/logo.png";
 
 const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
         <li><Link to="/Activity">Activity</Link></li>
-        <li><Link to="/Rewards">Rewards</Link></li>
+        <li>
+            <Link to="/"><img src={GreenSteps_Logo.png} alt="Home" style={{ height: "35px", width: "auto" }} /></Link>
+        </li>
+        {/* <li><Link to="/">Logo</Link></li> */}
+        <li><Link to="/Login">Login</Link></li>
       </ul>
     </nav>
   );
 };
 
 export default Navbar;
+
+
