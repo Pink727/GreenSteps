88cb75a3c6d398859afae8f221ab3a993f7364b3
Pink727 <doc72789@gmail.com>
2025-03-03 15:43:02 -0800

    feat: add registration route and component, update Login page link

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
 import Activities from './pages/Activity';
+import Register from './pages/Register'; // Import Register component
 import Footer from './components/Footer';
 import './styles.css';
 
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/activities" element={<Activities />} />
+        <Route path="/register" element={<Register />} /> {/* Add Register route */}
       </Routes>
       <Footer />
     </Router>
