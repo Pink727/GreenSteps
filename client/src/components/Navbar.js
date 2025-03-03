6a46d59ed16a68c1f7b783488060e40ec27a2d29
Dee Castle <doc72789@gmail.com>
2025-03-03 12:18:58 -0800

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "/logo.png";
+import logo from "../assets/GreenSteps_Logo.png";
 
 const Navbar = () => {
   return (
@@ -8,7 +8,7 @@ const Navbar = () => {
       <ul>
         <li><Link to="/Activity">Activity</Link></li>
         <li>
-            <Link to="/"><img src={GreenSteps_Logo.png} alt="Home" style={{ height: "35px", width: "auto" }} /></Link>
+            <Link to="/"><img src={logo} alt="Home" style={{ height: "35px", width: "auto" }} /></Link>
         </li>
         {/* <li><Link to="/">Logo</Link></li> */}
         <li><Link to="/Login">Login</Link></li>
